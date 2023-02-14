fe8ef3fa1bc8c612e44070fd7917abe45590090c
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2023-02-15 01:45:10 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { GlobalStyle } from './globalStyle';
-import { useAppDispatch, useAppSelector } from './hooks';
+import { useAppDispatch } from './hooks';
 import { getGoodsFetch } from './redux';
 import { useEffect } from 'react';
 
 function App() {
   const dispatch = useAppDispatch();
-  const goodsList = useAppSelector(state => state.commodityState.goodsList);
   useEffect(() => {
     dispatch(getGoodsFetch());
   }, [dispatch]);
-  console.log(goodsList);
 
   return (
     <div>
