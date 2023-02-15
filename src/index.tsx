2190a53de6c898dd26974ccdb7c349696fdaef06
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2023-02-15 12:02:00 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,18 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux';
 import { PersistGate } from 'redux-persist/integration/react';
-// import { persistor } from './redux/store';
-
+import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <BrowserRouter basename="/shop">
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
